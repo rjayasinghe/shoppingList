b6238ac31ea4042017246bb069e353b6cd3ab82c
Robin De Silva Jayasinghe <robin@jayasinghe.de>
2015-05-06 16:45:08 +0200

    Can delete items from list now
    Added UIDs to items. Added bower.json for client side dependencies.

--- client/shop.js
+++ client/shop.js
@@ -3,16 +3,21 @@
 
     app.controller('ShopController', function() {
         this.items = [];
-        this.deleteItem = function(item) {
-
+        this.deleteItem = function(item, items) {
+            items.forEach(function(element, index, array) {
+                if(element.uid === item.uid) {
+                    items.splice(index, 1);
+                    return;
+                }
+            });
         };
     });
 
     app.controller("NewItemController", function() {
-        this.newItem = {name:"", shop: "Rewe", quantity:1};
+        this.newItem = {name:"", shop: "Rewe", quantity:1, uid:nextUid()};
         this.addItem = function(items) {
             items.push(this.newItem);
-            this.newItem = {name:"", shop:"Rewe", quantity:1};
+            this.newItem = {name:"", shop:"Rewe", quantity:1, uid : nextUid()};
         };
     });
 })();
