e39289406cbb04b964b2a318ac4d46adc522e40b
Robin De Silva Jayasinghe <robin@jayasinghe.de>
2015-05-06 16:56:49 +0200

    stripped trailing semicolons

--- client/shop.js
+++ client/shop.js
@@ -1,23 +1,23 @@
 (function() {
-    var app = angular.module('shoppingList', []);
+    var app = angular.module('shoppingList', [])
 
     app.controller('ShopController', function() {
-        this.items = [];
+        this.items = []
         this.deleteItem = function(item, items) {
             items.forEach(function(element, index, array) {
                 if(element.uid === item.uid) {
-                    items.splice(index, 1);
-                    return;
+                    items.splice(index, 1)
+                    return
                 }
-            });
-        };
-    });
+            })
+        }
+    })
 
     app.controller("NewItemController", function() {
-        this.newItem = {name:"", shop: "Rewe", quantity:1, uid:nextUid()};
+        this.newItem = {name:"", shop: "Rewe", quantity:1, uid:nextUid()}
         this.addItem = function(items) {
-            items.push(this.newItem);
-            this.newItem = {name:"", shop:"Rewe", quantity:1, uid : nextUid()};
-        };
-    });
-})();
+            items.push(this.newItem)
+            this.newItem = {name:"", shop:"Rewe", quantity:1, uid : nextUid()}
+        }
+    })
+})()
