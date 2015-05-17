d2e0597149627dfb1f054f03d34e733a3d59d8af
Robin De Silva Jayasinghe <robin@jayasinghe.de>
2015-05-17 23:40:45 +0200

    Connected angular controllers with pouchdb
    Still some work to do. need to refresh the list
    after adding items and enable deletion of items.

--- client/shop.js
+++ client/shop.js
@@ -1,11 +1,26 @@
 (function() {
     var app = angular.module('shoppingList', [])
 
+    var db = new PouchDB('http://192.168.66.98:5984/shopping')
+
     app.controller('ShopController', function() {
-        this.items = []
+        var that = this
+        that.items = initData()
+        function initData() {
+            return db.allDocs({include_docs:true}).then(function(result) {
+                that.items = []
+                for(i=0; i < result.rows.length; i++) {
+                    that.items.push(result.rows[i].doc)
+                }
+                return that.items
+            }).catch(function(error) {
+                alert(error)
+            })
+        }
+
         this.deleteItem = function(item, items) {
             items.forEach(function(element, index, array) {
-                if(element.uid === item.uid) {
+                if(element._id === item._id) {
                     items.splice(index, 1)
                     return
                 }
@@ -14,10 +29,11 @@
     })
 
     app.controller("NewItemController", function() {
-        this.newItem = {name:"", shop: "Rewe", quantity:1, uid:nextUid()}
-        this.addItem = function(items) {
-            items.push(this.newItem)
-            this.newItem = {name:"", shop:"Rewe", quantity:1, uid : nextUid()}
+        this.newItem = {name:"", shop: "Rewe", quantity:1}
+
+        this.addItem = function() {
+            db.post(this.newItem)
+            this.newItem = {name:"", shop:"Rewe", quantity:1}
         }
     })
 })()
